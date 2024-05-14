65a1fba1015f90f5520ed3f04d3316c8033c6502
JustBodya <bagdancanturk@gmail.com>
2024-05-14 20:03:50 +0300

    upgrade

--- api/models.py
+++ api/models.py
@@ -5,9 +5,21 @@ class User(models.Model):
     password = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+    person = models.ForeignKey('People', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.username
+    
+    
+class People(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Роль'
+        verbose_name_plural = 'Роли'
 
 
 class Product(models.Model):
--- api/views.py
+++ api/views.py
@@ -1,13 +1,23 @@
-from .models import Product
+from .models import Product, Category
 from .serializers import ProductSerializer
-from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
-class ProductAPIList(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
+class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
 
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
 
-class ProductAPIUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+        if not pk:
+            return Product.objects.all()[:5]
+        
+        return Product.objects.filter(pk=pk)
+
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
