6c67794e47309681e0e4f48661303dca9ec85d42
JustBodya <bagdancanturk@gmail.com>
2024-05-14 19:10:38 +0300

    upgrade

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class ProductAPIList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductAPIUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
