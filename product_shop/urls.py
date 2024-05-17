bd9db0fd8f2bbeae61c9381f1bedaef31dba288f
JustBodya <bagdancanturk@gmail.com>
2024-05-17 15:21:35 +0300

    upgrade

--- product_shop/urls.py
+++ product_shop/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 from api.views import *
 
@@ -7,8 +7,6 @@ from api.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
-    path('api/v1/auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('api/v1/product/', ProductAPIList.as_view()),
     path('api/v1/product/<int:pk>/', ProductAPIUpdate.as_view()),
     path('api/v1/productdelete/<int:pk>/', ProductAPIDestroy.as_view()),
